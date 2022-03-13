175e3c49ea3ae78495b4be6dd3cdf358f653df4b
TongMinhDuong <101384330+TongMinhDuong@users.noreply.github.com>
2022-03-13 12:44:45 +0700

    Add files via upload

---
+++ chu vi hinh tron.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	float pi,C,r,S;
+	pi = 3.14;
+	r = 12.5;
+	S = r * r * pi;
+	C = r * 2 * pi;
+	
+	printf(" Chu vi C = %f\n", C);
+	
+	printf(" Dien tich S = %f ", S);
+	
+	return 0;
+}
